9c58ae12f9553a413c6327b1fdef1502e99b34e1
Vasiliy Bannikov <vab-999@ya.ru>
2019-11-23 20:27:36 +0500

    проверяем что генерим нормалную колоду

--- src/cards/deck.rs
+++ src/cards/deck.rs
@@ -132,12 +132,32 @@ impl Rank {
 impl Suit {
     fn enumerate() -> &'static [Suit; 4] {
         use Suit::*;
-        &[
-            Spades, Clubs, Diamonds, Hearts
-        ]
+        &[Spades, Clubs, Diamonds, Hearts]
     }
 }
 
+pub struct Deck([Card; 52]);
+use rand::prelude::*;
+impl Deck {
+    pub fn cards(&self) -> &[Card; 52] {
+        &self.0
+    }
+
+    pub fn shuffle<R: Rng>(rand: &mut R) -> Deck {
+        let mut cards: Vec<Card> = Vec::with_capacity(52);
+        for suit in Suit::enumerate() {
+            for rank in Rank::enumerate() {
+                cards.push(rank.of(*suit));
+            }
+        }
+        cards.shuffle(rand);
+        use Rank::*;
+        use Suit::*;
+        let mut arr: [Card; 52] = [Ace.of(Spades); 52];
+        arr.copy_from_slice(cards.as_slice());
+        Deck(arr)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::Rank::*;
@@ -157,5 +177,18 @@ mod tests {
         let a = Rank::enumerate().as_ptr();
         let b = Rank::enumerate().as_ptr();
         assert!(a == b);
+    }
+
+    #[test]
+    fn ok_shuffle() {
+        use std::collections::HashSet;
+        let mut rand = rand::thread_rng();
+        let deck = Deck::shuffle(&mut rand);
+        let cards = deck.cards();
+        let mut set: HashSet<_> = HashSet::default();
+        for card in cards.iter() {
+            set.insert(card);
+        }
+        assert!(set.len() == cards.len());
     }
 }
